d833d28b691e4ee50662556e00888aebe66f014d
qianzhihao <408985936@qqc.om>
2017-01-19 00:28:33 +0800

    登陆界面加入

---
+++ src/main/java/cn/sq/service/UserService.java
@@ -0,0 +1,10 @@
+package cn.sq.service;
+
+import cn.sq.domain.User;
+
+/**
+ * Created by qianzhihao on 2017/1/17.
+ */
+public interface UserService extends BaseService<User>{
+    void add(User user)throws Exception;
+}
